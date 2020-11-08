e560b2d675d5cd81ca4118b4e3254d83d3d36c49
Aliaksandr Zakharenka <mail@al3xable.me>
2020-11-09 00:02:49 +0300

    rpc lib

--- librpc/rpc.lua
+++ librpc/rpc.lua
@@ -1,32 +1,34 @@
 local thread = require('thread')
-local modem = require('modem')
+local component = require('component')
 local event = require('event')
+
+local modem = component.modem
 
 local rpc = {}
 
 -- Ports
-local rpc.PORT_CLIENT = 9501
-local rpc.PORT_SERVER = 9500
+rpc.PORT_CLIENT = 9501
+rpc.PORT_SERVER = 9500
 
 -- Commands
-local rpc.CMD_DISCOVER = 0 -- discover client name
-local rpc.CMD_EVAL = 1 -- execule code
+rpc.CMD_DISCOVER = 0 -- discover client name
+rpc.CMD_EVAL = 1 -- execule code
 
 -- Server answers
-local rpc.RPL_CMD_RECIEVED = 0 -- command recieved
-local rpc.RPL_CMD_RESULT = 1 -- command execution result code
-local rpc.RPL_CMD_DATA = 2 -- command data
+rpc.RPL_CMD_RECIEVED = 0 -- command recieved
+rpc.RPL_CMD_RESULT = 1 -- command execution result code
+rpc.RPL_CMD_DATA = 2 -- command data
 
-rpc.clientOpen = function()
+function rpc.clientOpen()
     event.listen('modem_message', rpc.clientHandler)
     return modem.isOpen(rpc.PORT_CLIENT) or modem.open(rpc.PORT_CLIENT)
 end
 
-rpc.call = function(cmd, data)
+function rpc.call(cmd, data)
     modem.broadcast(rpc.PORT_SERVER, math.random(0,65535), cmd, data)
 end
 
-rpc.clientHandler = function(receiverAddress, senderAddress, port, distance, data)
+function rpc.clientHandler(receiverAddress, senderAddress, port, distance, data)
     print(recieverAddress .. ' ' .. senderAddress)
 end
 
