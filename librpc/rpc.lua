50b2c4c9ffe32108ff837ab0c8a588957bda9943
Aliaksandr Zakharenka <mail@al3xable.me>
2020-11-08 21:27:28 +0300

    rpc lib

--- librpc/rpc.lua
+++ librpc/rpc.lua
@@ -1,7 +1,33 @@
+local thread = require('thread')
+local modem = require('modem')
+local event = require('event')
+
 local rpc = {}
 
-function rpc.call()
-    print('Hello from RPC')
+-- Ports
+local rpc.PORT_CLIENT = 9501
+local rpc.PORT_SERVER = 9500
+
+-- Commands
+local rpc.CMD_DISCOVER = 0 -- discover client name
+local rpc.CMD_EVAL = 1 -- execule code
+
+-- Server answers
+local rpc.RPL_CMD_RECIEVED = 0 -- command recieved
+local rpc.RPL_CMD_RESULT = 1 -- command execution result code
+local rpc.RPL_CMD_DATA = 2 -- command data
+
+function rpc.clientOpen()
+    event.listen('modem_message', rpc.clientHandler)
+    return modem.isOpen(rpc.PORT_CLIENT) or modem.open(rpc.PORT_CLIENT)
+end
+
+function rpc.call(cmd, data)
+    modem.broadcast(rpc.PORT_SERVER, math.random(0,65535), cmd, data)
+end
+
+function rpc.clientHandler(receiverAddress, senderAddress, port, distance, data)
+    print(recieverAddress .. ' ' .. senderAddress)
 end
 
 return rpc
