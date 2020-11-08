40616987e523818596d8816c7fa6854c265869ae
Aliaksandr Zakharenka <mail@al3xable.me>
2020-11-09 01:04:08 +0300

    rpc lib

--- librpc/rpc.lua
+++ librpc/rpc.lua
@@ -22,13 +22,21 @@ rpc.RPL_CMD_RESULT = 1 -- command execution result code
 rpc.RPL_CMD_DATA = 2 -- command data
 
 function rpc.clientOpen()
-    event.listen('modem_message', rpc.clientHandler)
+    rpc.clientPuller = thread.create(function()
+        local receiverAddress, senderAddress, port, distance, cmdId, code, data = event.pull('modem_message')
+        rpc.answers[cmdId] = 123
+    end)
     return modem.isOpen(rpc.PORT_CLIENT) or modem.open(rpc.PORT_CLIENT)
 end
 
-function rpc.call(cmd, data)
+function rpc.call(cmd, data, timeout)
+    if timeout == nil then
+        timeout = 30
+    end
     local cmdId = math.random(0,65535)
+
     print(cmdId)
+
     modem.broadcast(rpc.PORT_SERVER, cmdId, cmd, data)
     while rpc.answers[cmdId] == nil do
         print(rpc.answers[cmdId])
